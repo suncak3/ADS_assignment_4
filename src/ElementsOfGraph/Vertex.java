186af03b7ea007f43582c9c25998e31a553b39e7
suncak3 <tignat69228@gmail.com>
2024-05-20 23:57:18 +0500

    I have created edge and vertex classes with its methods, getters and setters, which are parts of the whole graph

---
+++ src/ElementsOfGraph/Vertex.java
@@ -0,0 +1,34 @@
+package ElementsOfGraph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+
+    public Vertex(V data){
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void setData(V data) {
+        this.data = data;
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    public void setAdjacentVertices(Map<Vertex<V>, Double> adjacentVertices) {
+        this.adjacentVertices = adjacentVertices;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight){
+        adjacentVertices.put(destination, weight);
+    }
+}
